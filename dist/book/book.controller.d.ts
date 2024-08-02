defaede9e1671e1ad4ca53d53a994c6ef75a8096
Shailendra Ydv <106907272+shailendra40@users.noreply.github.com>
2024-08-02 22:29:45 +0545

    upload project nest.js

---
+++ dist/book/book.controller.d.ts
@@ -0,0 +1,3 @@
+export declare class BookControlller {
+    findBookById(id: number): string;
+}
---
+++ dist/book/book.controller.js
@@ -0,0 +1,34 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BookControlller = void 0;
+const common_1 = require("@nestjs/common");
+let BookControlller = class BookControlller {
+    findBookById(id) {
+        console.log(id, typeof ("id"));
+        return "book by id";
+    }
+};
+exports.BookControlller = BookControlller;
+__decorate([
+    (0, common_1.Get)("/:id"),
+    __param(0, (0, common_1.Param)("id", common_1.ParseIntPipe)),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", String)
+], BookControlller.prototype, "findBookById", null);
+exports.BookControlller = BookControlller = __decorate([
+    (0, common_1.Controller)("book")
+], BookControlller);
+//# sourceMappingURL=book.controller.js.map
